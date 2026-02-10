d57ce20a5ce15b8d2799dd09c2d0e88d2c6555c2
hyojae02 <ansgywo0617@naver.com>
2026-02-10 15:14:49 +0900

    chore: Initial project setup

---
+++ src/main/java/com/example/mediwalk_be/MediwalkBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediwalk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediwalkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediwalkBeApplication.class, args);
+	}
+
+}
